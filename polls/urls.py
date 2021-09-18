471a9a7963eb08c16d5ad2a2e6f03c57b57ef17d
Taweesap Inliang <noom.taweesap@gmail.com>
2021-09-18 23:04:16 +0700

    Finished iteration2 part 4 redirect to index page

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-
 from . import views
+from django.http import HttpResponse
+from .models import Question
 
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
