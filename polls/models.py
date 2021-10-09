ab65b5e6e50671194f8840d426309f6299180228
Taweesap Inliang <noom.taweesap@gmail.com>
2021-10-09 23:48:12 +0700

    Fix more bug.

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('Expiration date', null=True)
+    end_date = models.DateTimeField('ending date for voting',
+                                    null=True, default=timezone.now)
 
     def __str__(self):
         """Return the question text.
