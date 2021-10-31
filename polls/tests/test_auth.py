0ddd4a6a08498aa894604420f941b7f13650e16e
Taweesap Inliang <noom.taweesap@gmail.com>
2021-10-31 23:54:52 +0700

    Fix test cases

--- polls/tests/test_auth.py
+++ polls/tests/test_auth.py
@@ -14,14 +14,15 @@ class AuthenticationTests(TestCase):
         }
         User.objects.create_user(**self.user)
 
-    def test_user_log_in(self):
+    def test_login(self):
         response = self.client.post(reverse('login'), self.user)
         self.assertEqual(response.status_code, 302)
         response = self.client.get(reverse('polls:index'))
         self.assertTrue(response.context['user'].is_authenticated)
 
-    def test_user_log_out(self):
+    def test_logout(self):
         self.client.post(reverse('login'), self.user)
         response = self.client.post(reverse('logout'))
         self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('polls:index'))
         self.assertFalse(response.context['user'].is_authenticated)
