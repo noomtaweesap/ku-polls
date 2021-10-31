5d927eb57435cc95573cf2dec2b93a086a38b561
Taweesap Inliang <noom.taweesap@gmail.com>
2021-11-01 00:02:39 +0700

    Fix bugs

--- polls/tests/test_models.py
+++ polls/tests/test_models.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-from .models import Question
+from ..models import Question
 from django.urls import reverse
 
 
